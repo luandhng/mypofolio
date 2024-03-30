817fb63d063a160d25be9d10e9c5082a3d8deac5
hluandev <97277916+hluandev@users.noreply.github.com>
2024-03-30 00:32:45 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <h1>This is cool</h1>
-      <p>Love is here</p>
-      <p>Love is here</p>
-      <p>Love is reaklly here</p>
-    </main>
-  );
+  return <main></main>;
 }
